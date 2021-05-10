5b864d8fc02f3a9d7fc2f0c48d6b976147fca58f
Ajain Vivek <ajainvivek07@gmail.com>
2021-05-10 17:21:59 +1000

    feat: deploy lambda

--- packages/service/core/src/index.ts
+++ packages/service/core/src/index.ts
@@ -7,7 +7,7 @@ import cors from '@koa/cors'
 import bodyParser from 'koa-bodyparser'
 import helmet from 'koa-helmet'
 import { AppContext } from './types.js'
-import { authRouter, otpRouter } from './routes/index.js'
+import defaultRouter, { authRouter, otpRouter } from './routes/index.js'
 import { config } from './config.js'
 
 const app = new Koa<AppContext>()
@@ -15,6 +15,8 @@ const app = new Koa<AppContext>()
 app.use(cors())
 app.use(bodyParser())
 app.use(helmet())
+
+app.use(defaultRouter.routes())
 
 app.use(authRouter.routes())
 app.use(authRouter.allowedMethods())
--- packages/service/core/src/routes/index.ts
+++ packages/service/core/src/routes/index.ts
@@ -1,2 +1,14 @@
+import Router from '@koa/router'
+import { AppRouter } from './../types'
+
+const router = new Router() as AppRouter
+
+router.get('/healthcheck', (ctx) => {
+    ctx.body = '👍';
+});
+
 export { router as authRouter } from './auth'
 export { router as otpRouter } from './otp'
+export default router
+
+
