34d446af2c49266bacf765f7e4f13b944f410bc6
Ajain Vivek <ajainvivek07@gmail.com>
2021-05-10 13:57:07 +1000

    feat: update region

--- ci/push-ecr.sh
+++ ci/push-ecr.sh
@@ -10,7 +10,7 @@ awsAccountId=$2
 image=$3
 
 # login to AWS ECR
-aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.region.amazonaws.com
+aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com
 # tag the docker image
 docker tag ${image} ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${image}
 # push the image to ECR
