f32d89319836745dee816217fd706b1c5e1df222
Ajain Vivek <ajainvivek07@gmail.com>
2021-05-12 13:34:03 +1000

    feat: create infrastructure for ecs

--- packages/service/core/src/auth/login.ts
+++ packages/service/core/src/auth/login.ts
@@ -3,8 +3,13 @@ import { Next } from 'koa'
 
 export async function getUser (ctx: AppContext, next: Next): Promise<void> {
   if (ctx.request.body.email != null) {
-    const { uid, displayName, email, emailVerified, phoneNumber } =
-      await ctx.services.auth.getUserByEmail(ctx.request.body.email as string)
+    const {
+      uid,
+      displayName,
+      email,
+      emailVerified,
+      phoneNumber
+    } = await ctx.services.auth.getUserByEmail(ctx.request.body.email as string)
 
     ctx.state.user = {
       uid,
@@ -16,10 +21,15 @@ export async function getUser (ctx: AppContext, next: Next): Promise<void> {
   }
 
   if (ctx.request.body.phoneNumber != null) {
-    const { uid, displayName, email, emailVerified, phoneNumber } =
-      await ctx.services.auth.getUserByEmail(
-        ctx.request.body.phoneNumber as string
-      )
+    const {
+      uid,
+      displayName,
+      email,
+      emailVerified,
+      phoneNumber
+    } = await ctx.services.auth.getUserByEmail(
+      ctx.request.body.phoneNumber as string
+    )
 
     ctx.state.user = {
       uid,
