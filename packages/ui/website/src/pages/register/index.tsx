598c5e9799cd56195c982a6c85337b01c018fea1
Ajain Vivek <ajainvivek07@gmail.com>
2021-05-13 15:21:55 +1000

    feat: add theme to register page

--- packages/ui/website/src/pages/register/index.tsx
+++ packages/ui/website/src/pages/register/index.tsx
@@ -1,21 +1,25 @@
 import React from "react"
 
+import ThemeProvider from "@material-ui/styles/ThemeProvider"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
+import theme from "../../theme"
 
 const Register = () => (
-  <Layout>
-    <SEO title="Register - Medical Practitioner" />
-    <div className="typeform-full">
-      <iframe
-        width="100%"
-        height="100%"
-        frameborder="0"
-        allow="camera; microphone; autoplay; encrypted-media;"
-        src="https://form.typeform.com/to/vPR89nv5?typeform-medium=embed-snippet"
-      ></iframe>
-    </div>
-  </Layout>
+  <ThemeProvider theme={theme}>
+    <Layout>
+      <SEO title="Register - Medical Practitioner" />
+      <div className="typeform-full">
+        <iframe
+          width="100%"
+          height="100%"
+          frameborder="0"
+          allow="camera; microphone; autoplay; encrypted-media;"
+          src="https://form.typeform.com/to/vPR89nv5?typeform-medium=embed-snippet"
+        ></iframe>
+      </div>
+    </Layout>
+  </ThemeProvider>
 )
 
 export default Register
