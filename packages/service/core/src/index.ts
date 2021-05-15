ee6599a6afc4b9c39edf3b5f546e712637f46754
Ajain <ajainvivek07@gmail.com>
2021-05-15 19:30:14 +1000

    feat: add swagger

--- packages/service/core/src/index.ts
+++ packages/service/core/src/index.ts
@@ -1,16 +1,36 @@
 import Koa from 'koa'
+import swaggerUi from 'koa2-swagger-ui'
 import cors from '@koa/cors'
 import bodyParser from 'koa-bodyparser'
 import helmet from 'koa-helmet'
+import yamljs from 'yamljs'
 import { AppContext } from './types.js'
 import defaultRouter, { authRouter, otpRouter } from './routes/index.js'
 import { config } from './config.js'
 
 export const app = new Koa<AppContext>()
 
+const { koaSwagger } = swaggerUi
+
+const spec = yamljs.load('./openapi.yaml')
+
+const UNPKG_SWAGGER_UI = 'cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.46.0/';
+const SELF = "'self'";
+
 app.use(cors())
 app.use(bodyParser())
-app.use(helmet())
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      baseUri: [SELF, UNPKG_SWAGGER_UI],
+      defaultSrc: [SELF, UNPKG_SWAGGER_UI],
+      styleSrc: [SELF, "'unsafe-inline'", UNPKG_SWAGGER_UI, 'fonts.googleapis.com'],
+      fontSrc: [SELF, 'fonts.googleapis.com', 'fonts.gstatic.com'],
+      scriptSrc: [SELF, "'unsafe-inline'", UNPKG_SWAGGER_UI],
+      imgSrc: [SELF, 'data:', UNPKG_SWAGGER_UI],
+    },
+  },
+}))
 
 app.use(defaultRouter.routes())
 
@@ -19,6 +39,15 @@ app.use(authRouter.allowedMethods())
 
 app.use(otpRouter.routes())
 app.use(otpRouter.allowedMethods())
+
+app.use(
+  koaSwagger({
+    routePrefix: '/swagger', // host at /swagger instead of default /docs
+    swaggerOptions: {
+      spec // example path to json
+    },
+  }),
+)
 
 const port = config.get('server.port')
 
